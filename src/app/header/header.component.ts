0aa69b872c43b510cf1fd56de08251db6b03fcb4
Mohammed tareq <Mohtareq1999m@gmail.com>
2025-05-30 15:11:40 +0300

    last update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,12 +9,16 @@ import { NavComponent } from '../nav/nav.component';
 })
 export class HeaderComponent {
 imgPath=["../../assets/images/lin.png",
-  "../../assets/images/git.png",
+  "../../assets/images/git (2).png",
   "../../assets/images/f2.png",
   "../../assets/images/logo 1.png",
   "../../assets/images/Rectangle-67.png",
   "../../assets/images/Rectangle 26.png",
   "../../assets/images/Group (1).png",
+  "../../assets/images/logo.png",
+  "../../assets/images/Vector.png",
+  "../../assets/images/IT.png",
+  "../../assets/images/my.png",
 ];
 
 
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Skill {
+  img: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
+  skills: Skill[] = [
+    { img: "assets/images/html.png", name: "HTML" },
+    { img: "assets/images/css.png", name: "CSS" },
+    { img: "assets/images/js (2).png", name: "JAVA SCRIPT" },
+    { img: "assets/images/sql.png", name: "MY SQL" },
+    { img: "assets/images/bt.png", name: "BOOTSTRAP" },
+    { img: "assets/images/php.jfif", name: "PHP" },
+    { img: "assets/images/lara.jpg", name: "LARAVEL" },
+    { img: "assets/images/an.jfif", name: "ANGULAR" }
+  ];
 
+  learn: Skill[] = [
+    { img: "assets/images/js (2).png", name: "Node JS" },
+    { img: "assets/images/sass.png", name: "SASS" },
+    { img: "assets/images/mon.png", name: "MONGO DB" },
+    { img: "assets/images/ts.png", name: "TYPE SCRIPT" }
+  ];
+
+  other: Skill[] = [
+    { img: "assets/images/c++.png", name: "C++" },
+    { img: "assets/images/git.png", name: "GIT" },
+    { img: "assets/images/linx.jpg", name: "LINUX" }
+  ];
 }
